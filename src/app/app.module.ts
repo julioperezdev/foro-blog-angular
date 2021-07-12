fa27c7dced931ceb10c55253e96f1a850263c5e7
julioperezdev <protobot.dev@gmail.com>
2021-07-11 22:41:24 -0300

    setup the environment

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IndexComponent } from './index/index.component';
+import { PublicBlogListComponent } from './public-blog-list/public-blog-list.component';
+import { CategorySelectorComponent } from './category-selector/category-selector.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    IndexComponent,
+    PublicBlogListComponent,
+    CategorySelectorComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/public-blog-list/public-blog-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-public-blog-list',
+  templateUrl: './public-blog-list.component.html',
+  styleUrls: ['./public-blog-list.component.css']
+})
+export class PublicBlogListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
